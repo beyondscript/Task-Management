663046f94912867d697734cef7050bce9dfe21a9
Beyondscript <nafiul1351@gmail.com>
2024-11-22 14:12:23 +0600

    18th commit

--- resources/js/components/errors/404.vue
+++ resources/js/components/errors/404.vue
@@ -8,7 +8,7 @@
 	                        <div style="text-align: center;">
 	                            <img style="width: 260px;" :src="'/images/404-not-found.webp'">
 	                            <h3 style="text-transform: uppercase; margin-bottom: 0;">Page Not Found</h3>
-	                            <p style="padding-bottom: 10px; padding-top: 5px; margin-bottom: 0;">The page you are looking for might have been removed or temporarily unavailable.</p>
+	                            <p style="padding-bottom: 10px; padding-left: 5px; padding-right: 5px; padding-top: 5px; margin-bottom: 0;">The page you are looking for might have been removed or temporarily unavailable.</p>
 	                            <a class="link" style="width: 130px !important; text-transform: uppercase;" :href="'/'">Go To Home</a>
 	                        </div>
 	                    </div>
