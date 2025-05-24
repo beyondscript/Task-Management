b8316f934c898c8fd77d66072dcf92551ca67de0
Beyondscript <nafiul1351@gmail.com>
2025-05-24 15:48:17 +0600

    21th commit

--- resources/views/admin/changePassword.blade.php
+++ resources/views/admin/changePassword.blade.php
@@ -71,7 +71,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
